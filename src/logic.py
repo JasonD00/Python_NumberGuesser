ce582163b96b6c55fcfd05f2707bccd81b2734b7
Jason Donnelly <JasonDonnelly00@gmail.com>
2025-08-26 23:19:34 +0100

    Number Guessing Game Works Now, may add a limit for guesses in th future

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,25 @@
 import random
 
 class NumberGuessingGame:
+    def __init__(self): # Constructor runs when a new game starts
+        self.reset_game() # Call reset to reset the game
 
+
+    def check_guess(self, guess):
+        if not str(guess).isdigit(): # Checks input is a number and returns message if not
+            return "Please enter a number", False
+
+        guess = int(guess) # Convert guess from String to int
+        self.attempts += 1 # Add one attempt
+
+        if guess < self.number_to_guess: # If guess is smaller, return message
+            return "Too low! Try again.", False
+        elif guess > self.number_to_guess: # If guess is higher, return message
+            return "Too high! Try again.", False
+        else: # Success message if number guessed is correct
+            return f"Correct! You guessed it in {self.attempts}, tries.", True
+
+    # Choose a random number between 1 and 100, keep track of attempts (base 0)
+    def reset_game(self):
+        self.number_to_guess = random.randint(1, 100)
+        self.attempts = 0
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 # lib for GUI elements, buttons etc..
 import tkinter as tk
+from logic import NumberGuessingGame
 
-
+game = NumberGuessingGame()
 
 
 # The GUI setup
@@ -10,23 +11,37 @@ root.title("Number Guessing Game")
 root.geometry("450x430")
 root.configure(bg="#1E1E2F")
 
+
+# Get the input from enter-box/input, call check_guess from logic
+# Green label text if correct, yellow if wrong
+def check_guess():
+    message, correct = game.check_guess(enter.get())
+    result_label.config(text=message, fg="#6BCB77" if correct else "#FFD93D")
+
+def reset_game():
+    game.reset_game() # Start a new game
+    enter.delete(0, tk.END) # Clear enter box
+    result_label.config(text="", fg="#FFFFFF") # Reset the result label
+
+
 # Widgets for labels and buttons..
 
 # Specify Label specifics like text, font style and color, bg of the label and the fg of the label
-title_label = tk.Label(root, text=" Number Guessing Game", font=("Arial Black", 20), bg="#1E1E2F", fg="#FFFFFF")
+title_label = tk.Label(root, text=" Number Guessing Game: 1-100", font=("Comic Sans MS", 20), bg="#2E2E3F", fg="#FFFFFF")
 title_label.pack(pady=20) # Vertical spacing fo 20 pixels from top to bottom with default pack behaviour
 
-enter = tk.Entry(root, font=("Arial", 14), justify="center", width=10, bg="#2E2E3F", fg="#FFFFFF", bd=0, insertbackground="#FFFFFF")
+enter = tk.Entry(root, font=("Comic Sans MS", 14), justify="center", width=10, bg="#2E2E3F", fg="#FFFFFF", bd=0, insertbackground="#FFFFFF")
 enter.pack(pady=10, padx=20)
 
 # bd = border, padx= space lft and right pady= top and bottom
-guess_button = tk.Button(root, text="Guess", font=("Arial", 12, "bold"), bg="#6BCB77", fg="#1E1E2F",activebackground="#58C470", bd=0, padx=20, pady=10)
+guess_button = tk.Button(root, text="Guess", font=("Comic Sans MS", 12, "bold"), bg="#6BCB77", fg="#1E1E2F",activebackground="#58C470", activeforeground="#FFFFFF", command=check_guess, bd=0, padx=20, pady=10)
 guess_button.pack(pady=10)
 
 
-reset_button = tk.Button(root, text="Reset", font=("Arial", 12, "bold"), bg="#FF6B6B", fg="#1E1E2F",activebackground="#FF4C4C", bd=0, padx=20, pady=10)
+reset_button = tk.Button(root, text="Reset", font=("Comic Sans MS", 12, "bold"), bg="#FF6B6B", fg="#1E1E2F",activebackground="#FF4C4C", activeforeground="#FFFFFF", command=reset_game, bd=0, padx=20, pady=10)
 reset_button.pack(pady=10)
 
-
+result_label = tk.Label(root, text="", font=("Comic Sans MS", 10),  bg="#1E1E2F", fg="#FFFFFF",)
+result_label.pack(pady=20)
 
 root.mainloop()
